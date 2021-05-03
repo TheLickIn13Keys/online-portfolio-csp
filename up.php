0f65fb1bf2dcb0d5f248837cdfda4424c80c403c
Bardia Anvari <bardiaanvari10@gmail.com>
2021-05-03 10:12:32 -0700

    Update up.php

--- up.php
+++ up.php
@@ -1,7 +1,7 @@
 <?php
-$secret_key = "KEYGOESHERE"; //Set this as your secret key, to prevent others uploading to your server.
+$secret_key = "@*[zRY<?6=.SvoP"; //Set this as your secret key, to prevent others uploading to your server.
 $sharexdir = "images/"; //This is your file dir, also the link..
-$domain_url = 'http://ss.yourdomain.com/'; //Add an S at the end of HTTP if you have a SSL certificate.
+$domain_url = 'http://ss.bardia.app/'; //Add an S at the end of HTTP if you have a SSL certificate.
 $lengthofstring = 5; //Length of the file name
 
 function RandomString($length) {
